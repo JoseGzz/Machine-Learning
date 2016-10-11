e1158cb770fd97a1190dcfa21aaf971b72f9665b
Jose Gzz <josegzza@gmail.com>
2016-10-11 00:19:26 -0500

    move sys.append up

--- ud120-projects/naive_bayes/nb_author_id.py
+++ ud120-projects/naive_bayes/nb_author_id.py
@@ -9,23 +9,21 @@
     Sara has label 0
     Chris has label 1
 """
-
+# imports
 from sklearn.naive_bayes import GaussianNB
 import sys
+sys.path.append("../tools/")
 from time import time
 from email_preprocess import preprocess
 from sklearn.metrics import accuracy_score
 
-sys.path.append("../tools/")
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
-t0 = time()
-
 clf = GaussianNB()
 
+t0 = time()
 clf.fit(features_train, labels_train)
 print "training time:", round(time()-t0, 3), "s"
 
